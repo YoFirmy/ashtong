e0a748ee9ac1d3e7a4b1c80e158365736400a8df
Matt Firmston <mattfirmston@gmail.com>
2021-03-01 20:39:37 +0000

    created sign up page, created notice and alerts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
 
+    validates :name, presence: true
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+    validates :password, presence: true, format: { with: /\A\S{5,}\z/ }
+
+    after_initialize do
+        self.admin ||= false
+    end
 end
