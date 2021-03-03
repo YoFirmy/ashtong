eaf0c2a67ec15920b9fc6321db415623e7524ffc
Matt Firmston <mattfirmston@gmail.com>
2021-03-03 13:51:19 +0000

    created sign in page, and account info page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,23 @@
 class SessionsController < ApplicationController
-  def sign_in
+  def new
   end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in succesfully."
+    else
+      flash[:alert] = "Invalid email or password."
+      render :new
+    end
+  end
+
+  def account
+  end
+  
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "logged out"
+    redirect_to root_path, notice: "Logged out successfully"
   end
 end
