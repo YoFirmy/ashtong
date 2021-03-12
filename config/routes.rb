5cf00b02d48220c8a5bd4d3c27e8e7013eb0f818
Matt Firmston <mattfirmston@gmail.com>
2021-03-12 16:42:23 +0000

    created product show page with links

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
   
   get "tongs", to: "products#index"
+  get "order_tong", to: "products#show"
 
   root to: "main#index"
 end
